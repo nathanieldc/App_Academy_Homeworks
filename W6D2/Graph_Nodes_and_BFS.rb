99c2475f539f4cb07a3f446f14f399e5aa0623b3
Nathaniel Cooke <nathanieldcooke1@gmail.com>
2019-08-06 07:54:01 -0700

    Graph Node is complete

---
+++ W6D2/Graph_Nodes_and_BFS.rb
@@ -0,0 +1,15 @@
+class GraphNode
+
+    attr_reader :val :neighbors
+
+    def initialize(val)
+        @val = val
+        @neighbors = []
+    end
+end
+
+
+### I undertsand this is essentailly nothing, but this is all I got to before class starts in 10mins
+### after class yesterday from 5:30p.m to around 8:30p.m I was doing the say befores homework
+### and correcting the A01 problems I got wrong form 8:30p.m to 11:00p.m I studied then 6a.m to now. 
+### it's taken me much longer than the estimated 40mins to understand the material. 
