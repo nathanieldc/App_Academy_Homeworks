b89796511bf6a3843a0083ac21e54e1433cd6caf
Nathaniel Cooke <nathanieldcooke1@gmail.com>
2019-09-10 09:05:30 -0700

    completed bfs homework

--- W6D2/Graph_Nodes_and_BFS.rb
+++ W6D2/Graph_Nodes_and_BFS.rb
@@ -1,28 +1,56 @@
+require "set"
+
+
+
 class GraphNode
 
     attr_accessor :val, :neighbors
 
     def initialize(val)
-        self.val = val
-        self.neighbors = []
+        @val = val
+        @neighbors = []
+    end
+
+    def add_neighbors(neighbor)
+        self.neighbors += neighbor
     end
 
 end
 
-def bfs(starting_node, target)
 
-    q = []
-    
+def bfs(starting_node, target)
+    visted = Set.new()
+    q = [starting_node]
+
+    until q.empty?
+
+        curr_node = q.shift
+        unless visted.include?(curr_node)
+            return curr_node.val if curr_node.val == target
+            q.push(*curr_node.neighbors)
+            visted.add(curr_node)
+        end
+    end
+    nil
 end
 
 
 
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
 
 
+a.add_neighbors([b, c, e])
+c.add_neighbors([b, d])
+e.add_neighbors([a])
+f.add_neighbors([e])
+
+puts bfs(a, "b")
+puts ""
+puts bfs(a, "f")
 
 
-
-### I undertsand this is essentailly nothing, but this is all I got to before class starts in 10mins
-### after class yesterday from 5:30p.m to around 8:30p.m I was doing the say befores homework
-### and correcting the A01 problems I got wrong form 8:30p.m to 11:00p.m I studied then 6a.m to now. 
-### it's taken me much longer than the estimated 40mins to understand the material. 
